37f962b9f359b8e4bc771d8125a3a434a18cb5fe
LHW-060805 <3421218133@qq.com>
2025-07-22 22:52:48 +0800

    test17

--- src/Study/StreamTest/Instantiation.java
+++ src/Study/StreamTest/Instantiation.java
@@ -4,7 +4,6 @@ import String_exercise.String_exercise.CampareTest.Product;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Stream;
 
 public class Instantiation {
     public static void main(String[] args) {
@@ -30,5 +29,30 @@ public class Instantiation {
         productList.stream().skip(3).forEach(System.out::println);
         System.out.println("===================================================");
         productList.stream().distinct().forEach(System.out::println);
+        List<String> list=Arrays.asList("AA","BB","CC");
+        //map是函数接口，可以用lambda表达式和方法引用
+        //map的作用是接收一个函数作为参数，将函数转换为其他形式或者提取，并且遍历到每个元素
+        list.stream().map(String::toLowerCase).toList().forEach(System.out::println);
+        System.out.println("===================================================");
+        //使用filter过滤掉数据后用map映射数据
+//        productList.stream().
+//                filter(product -> product.getName().length()>5).
+//                map(Product::getName).
+//                forEach(System.out::println);
+        productList.stream().map(Product::getName).filter(name->name.length()>3).forEach(System.out::println);
+        System.out.println("===================================================");
+        Integer[] arr1={54,98,100,33,4,86,0};
+        String[] arr2={"AA","ZZ","DD","赵声阁"};
+//        Arrays.stream(arr1).sorted().forEach(System.out::println);
+//        Arrays.stream(arr2).sorted().forEach(System.out::println);
+        productList.stream().sorted().forEach(System.out::println);
+        Arrays.stream(arr2).sorted((e1,e2)->-e1.compareTo(e2)).forEach(System.out::println);
+
+        System.out.println("===================================================");
+        System.out.println(Arrays.stream(arr1).reduce(0, Integer::max));
+//        System.out.println(Arrays.stream(arr1).max(Integer::compare).get());
+        System.out.println(productList.stream().max(Product::compareTo));
+        long count = productList.stream().count();
+        System.out.println(count);
     }
 }
